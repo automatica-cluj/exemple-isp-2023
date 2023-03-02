9e10254f703f04cdf65eb78bbd32b98535ef4a16
Mihai <mihai.hulea@aut.utcluj.ro>
2023-03-02 15:31:12 +0200

    Exemplu hello world curs 1.

---
+++ exmeplul-1-curs-cj/src/main/java/utcluj/isp/curs1/HelloWorld.java
@@ -0,0 +1,13 @@
+package utcluj.isp.curs1;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
