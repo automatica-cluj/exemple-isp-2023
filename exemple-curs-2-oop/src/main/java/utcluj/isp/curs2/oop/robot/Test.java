1cec99311794b43ab71831988902f6831229318d
Mihai <mihai.hulea@aut.utcluj.ro>
2023-03-23 15:00:39 +0200

    Exemple curs 4

---
+++ exemple-curs-2-oop/src/main/java/utcluj/isp/curs2/oop/robot/Test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utcluj.isp.curs2.oop.robot;
+
+/**
+ *
+ * @author mihai
+ */
+public class Test {
+    
+}
---
+++ exemple-curs4-oopdetails/src/main/java/utcluj/isp/curs3/oop/inner2annon/AnonymousInnerClassExample.java
@@ -0,0 +1,19 @@
+package utcluj.isp.curs3.oop.inner2annon;
+
+interface Greeting {
+    void greet();
+}
+
+class AnonymousInnerClassExample {
+    public static void main(String[] args) {
+
+        Greeting greeting = new Greeting() {
+            @Override
+            public void greet() {
+                System.out.println("Hello, world!");
+            }
+        };
+
+        greeting.greet();
+    }
+}
